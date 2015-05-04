b76de85e27809d62553531155dd3a46022fb720a
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-05-04 18:13:19 +0100

    long to short done

--- StringSorter.java
+++ StringSorter.java
@@ -20,22 +20,21 @@ public class StringSorter {
 		String [] strArray  = {"batman", "superman", "wonder woman", "martian manhunter",
 			"black canary", "green arrow", "the atom", "the flash"};
 			
-		StringComparator length = (t1, t2) -> t1.length() - t2.length();	
-		printArray(strArray,"In Length Order", length)
+		StringComparator short2Long = (t1, t2) -> t1.length() - t2.length();	
+		printArray(strArray,"In Length Order: shortest to longest", short2Long);
 		
+		StringComparator long2short = (t1, t2) -> t2.length() - t1.length();
+		printArray(strArray,"In Length Order: longest to shortest", long2short);
 
 	}
 	
-	private void printArray(String strArray, String orderType, StringComparator comparator) {
+	private static void printArray(String[] strArray, String orderType, StringComparator comparator) {
 		System.out.println(orderType);
 		List<String> list = Arrays.asList(strArray);
-		Collections.sort(list, length);
+		Collections.sort(list, comparator);
 		Object[] o = list.toArray();
 		for (Object element : o) {
 			System.out.println(element);
 		}
 	}
-		
-	
-	
 }
