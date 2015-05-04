284c70f3e7e7e61ddb7a1bf26bf42c8f88c0d2d4
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-05-04 21:05:54 +0100

    Q3 done

--- BetterString.java
+++ BetterString.java
@@ -10,8 +10,12 @@ interface TwoStringPredicate {
 
 public class BetterString {
 
+	public static void main(String[] args) {
+		System.out.println(betterString("1", "2", (s1, s2) -> s1.length() > s2.length()));
+		System.out.println(betterString("1", "2  ", (s1, s2) -> true));
+	}
 	
-	public String betterString(String s1, String s2, TwoStringPredicate lambda) {
+	public static String betterString(String s1, String s2, TwoStringPredicate lambda) {
 		String result = null;
 		if (lambda.test(s1, s2)) {
 			result = s1;
