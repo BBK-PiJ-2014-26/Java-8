b19ebaca52731dea6090f8eba7050ef2b1681e58
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-05-04 21:37:51 +0100

    Q5 solved

--- AllMatches.java
+++ AllMatches.java
@@ -1,6 +1,8 @@
 import java.util.function.Predicate;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Arrays;
 
 /**
  * Answers Q5 lambda expressions.
@@ -8,20 +10,23 @@ import java.util.ArrayList;
 public class AllMatches {
 	
 	public static void main(String[] args) {
-		String[] strArray = {"Hello", "Belgium", "boxing", "toe", "me", "I", "Yosemite", "car"};
-		List<String> words = Arrays.asList("Hello", "Belgium", "boxing", "toe", "me", "I", "Yosemite", "car");
+		List<String> words = Arrays.asList("Hello", "Belgium", "boxing", "toe", "me", "I", "Yosemite", "car", "bullion");
 		List<String> shortWords = allMatches(words, s -> s.length() < 4);
+		System.out.println(shortWords + "\n");
 		List<String> wordsWithB = allMatches(words, s -> s.contains("b"));
+		System.out.println(wordsWithB + "\n");	
 		List<String> evenLengthWords = allMatches(words, s -> (s.length() % 2) == 0);
+		System.out.println(evenLengthWords + "\n");
 	}
 	
 	
 	public static List<String> allMatches(List<String> list, Predicate<String> predicate) {
-		List<String> result = new ArrayList();
-		String[] temp = list.toArray();
-		for (String e : temp) {
-			if (predicate.test()) {
-				result.add(e);
+		List<String> result = new ArrayList<String>();
+		ListIterator<String> iterator = list.listIterator();
+		while (iterator.hasNext()) {
+			String temp = iterator.next();
+			if (predicate.test(temp)) {
+				result.add(temp);
 			}
 		}
 		return result;
